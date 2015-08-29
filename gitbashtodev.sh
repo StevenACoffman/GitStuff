bc6109493438e7ac440f13b15b3dc03bd56b226d
Steve Coffman <gears@umich.edu>
2015-08-29 15:44:22 -0400

    Initial add

---
+++ gitbashtodev.sh
@@ -0,0 +1,60 @@
+Applicant ID:   11680
+Applicant ID:   119390
+Applicant ID:   14350
+Applicant ID:   15899
+Applicant ID:   171907
+Applicant ID:   224736
+Applicant ID:   34910
+Applicant ID:   425094
+Applicant ID:   429555
+Applicant ID:   450470
+Applicant ID:   471438
+Applicant ID:   488911
+Applicant ID:   494079
+Applicant ID:   494264
+Applicant ID:   495597
+Applicant ID:   495650
+Applicant ID:   497117
+Applicant ID:   498093
+Applicant ID:   6958
+Applicant ID:   7946
+Adam York
+Alexander Mudar
+Anthony Daniels
+Bienvenido Gonzales
+Christopher Myers
+David Greer
+Eric Manzitti
+Frank Benedetti
+James White
+Joshua Walker
+Julie Keros
+Kerry Colligan
+Mohit Rupani
+Mukul Samak
+Namita Bhargava
+Pushyami Gundala
+Rhymney Weidner
+Sean Meyer
+Sireesha Polina
+Yunkyu Kwak
+You can view Adam York's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=450470&authType=1O page.
+You can view Alexander Mudar's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=471438&authType=1O page.
+You can view Anthony Daniels's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=7946&authType=1O page.
+You can view Bienvenido Gonzales's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=495597&authType=1O page.
+You can view Christopher Myers's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=11680&authType=1O page.
+You can view David Greer's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=119390&authType=1O page.
+You can view Eric Manzitti's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=497117&authType=1O page.
+You can view Frank Benedetti's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=6958&authType=1O page.
+You can view James White's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=171907&authType=1O page.
+You can view Joshua Walker's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=494264&authType=1O page.
+You can view Julie Keros's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=15899&authType=1O page.
+You can view Kerry Colligan's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=14350&authType=1O page.
+You can view Mohit Rupani's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=429555&authType=1O page.
+You can view Mukul Samak's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=498093&authType=1O page.
+You can view Namita Bhargava's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=488911&authType=1O page.
+You can view Pushyami Gundala's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=494079&authType=1O page.
+You can view Rhymney Weidner's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=425094&authType=1O page.
+You can view Sean Meyer's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=224736&authType=1O page.
+You can view Sireesha Polina's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=495650&authType=1O page.
+You can view Yunkyu Kwak's profile from https://heprod.dsc.umich.edu/psp/heprodop/EMPLOYEE/HRMS/c/HRS_HRAT.HRS_MANAGE_APP.GBL?Page=HRS_MANAGE_APP&Action=U&HRS_PERSON_ID=34910&authType=1O page.
